cd8c3f6222f1db9ab317e839107355dd8f9b4306
raz421 <maharazbhuiyan3@gmail.com>
2025-04-27 14:26:43 +0600

    function and scope end

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,15 +1,36 @@
-//* scope
-// const obj = {
-//   a: 7,
-// };
-// obj.a = 8;
+// function
+const a = function (b, c) {
+  return b + c;
+};
+console.log(a(6, 7));
 
-// console.log(obj.a);
+//arrowfunction
+let multi = (a, b) => {
+  console.log("multification happend here");
+  return a * b;
+};
+console.log(multi(8, 9));
 
-//* function
+// retun object in arrow function
 
-function sum(a, b) {
-  console.log(a + b);
-}
-const a = sum(7, 8);
-console.log(a);
+const obj = (a, b) => ({
+  a: a,
+  b: b,
+});
+console.log(obj(6, 7));
+
+// another way
+const obj2 = (a, b) => {
+  return {
+    a: a,
+    b: b,
+  };
+};
+console.log(obj2(10, 12));
+
+// annonymous function
+const fun = function () {
+  return () => {
+    console.log("hellow world ");
+  };
+};
