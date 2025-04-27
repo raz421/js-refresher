302163d78307fc7fa7c7615a9bc290fe45f5fd67
raz421 <maharazbhuiyan3@gmail.com>
2025-04-27 14:06:27 +0600

    function and scope initially

---
+++ js referesher/index.js
@@ -0,0 +1,15 @@
+//* scope
+// const obj = {
+//   a: 7,
+// };
+// obj.a = 8;
+
+// console.log(obj.a);
+
+//* function
+
+function sum(a, b) {
+  console.log(a + b);
+}
+const a = sum(7, 8);
+console.log(a);
