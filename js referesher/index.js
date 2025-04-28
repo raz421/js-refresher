f86b0737081a281b56732b517bb01ff2c766ba5d
raz421 <maharazbhuiyan3@gmail.com>
2025-04-28 13:56:12 +0600

    rest oparator added

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,16 +1,7 @@
-// nested spread oparator
-// in array of object
-const arr1 = [
-  { id: 1, name: "Meharaz" },
-  { id: 2, name: "Rahim" },
-];
-const arr2 = [...arr1];
-console.log(arr1);
-console.log(arr2);
-arr2[0].name = "Rohit";
-arr2[0].id = 45;
-// the nested level here means object in array not perfactly copy,because when it's spread
-// it's share reference.so one is change it's affect to other
-console.log(arr1);
-console.log(arr2);
-// so we can say spread copy in first level
+// rest oparator
+function Multi(a, ...rest) {
+  console.log(rest);
+  const ans = rest.reduce((acc, curr) => acc * curr, 1);
+  console.log(`${a}${ans}`);
+}
+Multi("the value is ", 89, 19, 45, 68);
