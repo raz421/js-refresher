4a97c84ece0ecc358869738d56b67e6269d9417b
raz421 <maharazbhuiyan3@gmail.com>
2025-04-28 13:44:26 +0600

    object trick added

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,27 +1,12 @@
-// for of and for in method
-
-// for of use for ittarable and for in use for non ittarable or object
-// use of forof
-// const num = [12, 13, 14];
-// let sum = 0;
-// for (let n of num) {
-//   console.log(n);
-// }
-// const str = "Meharaz";
-// for (let s of str) {
-//   console.log(s);
-// }
-
-//use of forin
-let obj = {
-  name: "Meharaz",
-  id: "221 - 15 - 5883",
-  age: 25,
+// object some trick
+let p = "property";
+const object = {
+  a: 43,
+  b: 98,
+  c: 80,
+  d: "d",
 };
-for (let key in obj) {
-    // here we cant access property like obj.key
-    // bexause obj.key	Always looks for a property named exactly "key" (not the variable's value).
-    // obj[key]	Looks for the property whose name is stored inside the key variable.
-
-  console.log(obj[key]);
-}
+object["p"] = 90;
+console.log(Object.keys(object));
+console.log(Object.values(object));
+console.log(Object.entries(object));
