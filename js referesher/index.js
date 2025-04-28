ad6ec4310ed2441818f2dbe5050f89356840889f
raz421 <maharazbhuiyan3@gmail.com>
2025-04-28 14:09:19 +0600

    truthy falsy value

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,7 +1,12 @@
-// rest oparator
-function Multi(a, ...rest) {
-  console.log(rest);
-  const ans = rest.reduce((acc, curr) => acc * curr, 1);
-  console.log(`${a}${ans}`);
+// truthy falsy value
+/*truthy means like a true falsy
+means like a false*/
+/* there are 6 type of truthy and falsy value present rest
+of other value is falsy*/
+// null,undefined,0,"",NaN,false
+const a = "";
+if (!a) {
+  console.log("the value is true");
+} else {
+  console.log("the value is false");
 }
-Multi("the value is ", 89, 19, 45, 68);
