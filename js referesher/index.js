f42761f89573a906ae5c18f2e2934a990001bae8
raz421 <maharazbhuiyan3@gmail.com>
2025-04-28 14:24:19 +0600

    some arry method explain

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,17 +1,32 @@
-// primitive
-let a = "javascript";
-let b = "go lang";
-a = b;
-b = "typescript";
-console.log(a);
-console.log(b);
+// array methods
+// find
+// const num = ["mango", "pineapple", "watermelon", "guava"];
+// const ans = num.find((f) => f === "peach" || f === "guava");
+// // if it's true it's give result
+// console.log(ans);
 
-// refernce type value
-let arr1 = [99, 89, 87];
-let arr2 = [35, 46, 78];
-arr1 = arr2;
-console.log(arr1);
-console.log(arr2);
-arr2.push(99);
-console.log(arr1);
-console.log(arr2);
+//findIndex
+// const res = num.findIndex((f) => f === "pineapple");
+// // if it's true it's give result index
+// console.log(res);
+
+// slice
+
+// const str = "Bangladesh is beatuifull country";
+// const slc = str.slice(0, 10);
+// console.log(slc);
+// console.log(str);
+
+// Slice another example
+// const num = [88, 99, 70, 30, 89, 99, 230, 80, 12, 34, 55, 667, 3];
+// const res = num.slice(0, 10);
+// console.log(res);
+
+// splice example
+const fruit = ["mango", "pineapple", "watermelon", "guava", "peach"];
+
+// const res=fruit.splice(startdeletenumber,how many remove,give value if any value need);
+
+const res = fruit.splice(2, 2, "jackfruit", "blckbery", "fruit3");
+console.log(res);
+console.log(fruit);
