89d2c731ef6cac2a518cb0ade972e55e235214b4
raz421 <maharazbhuiyan3@gmail.com>
2025-04-29 18:26:03 +0600

    problem solved in map

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,16 +1,37 @@
-// filter the prime number from an array
-const array = [2, 3, 4, 5, 6, 7, 9, 17];
+// map in array
+/*map transform a value into array elementt,map gives a new array
+this array length same as main arry but it's not manipulate main array.
+map transform value based on any action,it's map every element of the array*/
 
-function PrimeNumber(num, idx, arr) {
-  if (num > 0) {
-    for (let i = 2; i < num; i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return num;
-  }
-}
+// 2
+const num2 = [23, 45, 56];
+const res = num2.map((n) => n * n);
+res.push(78);
+console.log(res);
+console.log(num2);
 
-const res = array.filter(PrimeNumber);
-console.log(...res);
+//3
+const fruits = ["apple", "bananna"];
+const frt = fruits.map((f) => f.toUpperCase());
+console.log(frt);
+
+// 4
+const info = [
+  { name: "jackFruit", id: 1 },
+  { name: "pineApple", id: 2 },
+  { name: "apple", id: 3 },
+];
+const result = info.map(({ name }) => name);
+console.log(result);
+// 5
+const info2 = [
+  { name: "jackFruit", id: 1 },
+  { name: "pineApple", id: 2 },
+  { name: "apple", id: 3 },
+];
+const result2 = info2.map(({ name, id }) => ({ [name]: id }));
+console.log(result2);
+// 6
+const arr2 = [3, 4, [7, 9], 34, 56];
+const answer = arr2.map((a) => a * 2);
+console.log(answer);
