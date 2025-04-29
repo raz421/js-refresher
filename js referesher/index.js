ce91451569b7742c5114dddd971f49810e48acef
raz421 <maharazbhuiyan3@gmail.com>
2025-04-29 17:48:16 +0600

    findIndex practice

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,14 +1,11 @@
-// a positive number that is also less than it's neighbour find fist element
-const num = [33, -1, 23, 17, 13, 12];
-const res = num
-  .filter((val) => val > 0)
-  .find((val, index, arr) => {
-    if (index > 0 && val >= arr[index - 1]) {
-      return false;
-    }
-    if (index < arr.length - 1 && val >= arr[index + 1]) {
-      return false;
-    }
-    return true;
-  });
+// uses of findIndex
+const arr = [1, 45, -2, 78, 90];
+const res = arr.filter((a) => a > 0).findIndex((a) => a > 50);
 console.log(res);
+const ans = arr.filter((a) => a > 0).findIndex((a) => a > 100);
+console.log(ans);
+
+// findindex in sparse array
+const arr1 = [2, 4, , , -1, -3, -5, 6, , 8];
+const res1 = arr1.filter((a) => a > 0).findIndex((a) => a > 7);
+console.log(res1);
