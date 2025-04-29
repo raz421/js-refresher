b0bd1ed1ca178cf973dd3885c95606d9d76ed52c
raz421 <maharazbhuiyan3@gmail.com>
2025-04-29 18:33:15 +0600

    prime number problem fixed

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,5 +1,5 @@
 // filter the prime number from an array
-const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const array = [2, 3, 4, 5, 6, 7, 9, 17];
 
 function PrimeNumber(num, idx, arr) {
   if (num > 0) {
@@ -7,8 +7,8 @@ function PrimeNumber(num, idx, arr) {
       if (num % i == 0) {
         return false;
       }
-      return true;
     }
+    return num;
   }
 }
 
