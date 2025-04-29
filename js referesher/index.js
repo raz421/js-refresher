1b7488fee8283d3445a6a7b12e4abe01a962a451
raz421 <maharazbhuiyan3@gmail.com>
2025-04-29 18:32:00 +0600

    practice js essentials

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,12 +1,15 @@
-// nalish qualacing
-// only works right side when value is null or undefined
-const n1 = undefined;
-console.log(n1 ?? "Meharaz");
-
-//only works right side  when value is falsy value
-const lang = "c++";
-console.log(lang || "javascript");
-
-//only works right side  when value is truthy value
-const lang2 = "c++";
-console.log(lang2 && "javascript");
+const inventory = [
+  { name: "cherry", quantity: "2kg" },
+  { name: "jackfruit", quantity: "5kg" },
+  { name: "strawbery", quantity: "7kg" },
+  { name: "strawbery", quantity: "4kg" },
+  { name: "strawbery", quantity: "3kg" },
+  { name: "apple", quantity: "7kg" },
+  { name: "orange", quantity: "7kg" },
+];
+function Fruit(f) {
+  const res = f.name === "strawbery";
+  return res;
+}
+const ans = inventory.find(Fruit);
+console.log(ans);
