d9c06c1ddbc912224e8fe2d41b1e555f2b4c84f1
raz421 <maharazbhuiyan3@gmail.com>
2025-04-30 16:32:36 +0600

    spread oparator use base of condition

--- js referesher/index.js
+++ js referesher/index.js
@@ -1,38 +1,12 @@
-// spread oparator apply in array object and function
-// in spread oparator here only 1 level copy perfectly
-// other level share refference,not fully copied
-// const Object = {
-//   name: "meharaz",
-//   id: "221-15-5883",
-//   address: {
-//     name: "dhaka",
-//   },
-// };
-// const Object2 = { ...Object };
-// Object2.name = "Rahim";
-// console.log(Object);
-// console.log(Object2);
-// Object2.address.name = "chitagong";
-// console.log(Object);
-// console.log(Object2);
-// here it copies only one layer
+// we can use conditionally spread oparator into object or array
+const iscondition = true;
+const arr = [12, 34, 56];
+const arr2 = [39, 90, 80, ...(iscondition ? arr : [])];
+console.log(arr2);
 
-// example
-// const arr = [1, 2, 3, 4];
-// const arr2 = [...arr];
-// console.log(arr);
-// console.log(arr2);
-// arr2.push(4);
-// console.log(arr);
-// console.log(arr2);
-// here it copy one level so two array change independently
-
-// combining array
-// const arr1 = [90, 89, 99, 90];
-// const arr2 = [94, 80, 995, 190];
-// const combine = [...arr1, ...arr2];
-// console.log(combine);
-// add spread with array element
-// const arr6 = [90, 80, 45, 35, 23];
-// const add = [23, 45, ...arr6];
-// console.log(add);
+const Student = {
+  name: "Meharaz",
+  id: "221-15-5883",
+  ...(iscondition ? { uni: "diu" } : {}),
+};
+console.log(Student);
