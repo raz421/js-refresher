f03c1c0b714caf754d9e2eeeeecbe3c224dc776c
raz421 <maharazbhuiyan3@gmail.com>
2025-05-02 15:05:23 +0600

    simple promise

--- js referesher/js asynchronous/async.js
+++ js referesher/js asynchronous/async.js
@@ -1,21 +1,11 @@
-const takeOrder = (customer, callback) => {
-  console.log(`take order from ${customer}`);
-  callback(customer);
-};
-
-function process(customer, callback) {
-  console.log(`process order for ${customer}`);
+// promise in js
+const promise = new Promise((res, rej) => {
   setTimeout(() => {
-    console.log("cooking complete");
-    console.log(`order process for ${customer}`);
-    callback(customer);
-  }, 3000);
-}
-const completeOrder = (customer) => {
-  console.log(`complete order for ${customer}`);
-};
-takeOrder("customer 1", (customer) => {
-  process(customer, (customer) => {
-    completeOrder(customer);
-  });
+    console.log("async task  is complete ");
+    res();
+  }, 2000);
+
+});
+promise.then(() => {
+  console.log("Promise is consumed");
 });
