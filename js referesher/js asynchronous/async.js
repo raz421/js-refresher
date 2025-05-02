4ff2650dd4e4f2d92c675958c38de24c4140d002
raz421 <maharazbhuiyan3@gmail.com>
2025-05-02 15:02:34 +0600

    try catch added in promise

--- js referesher/js asynchronous/async.js
+++ js referesher/js asynchronous/async.js
@@ -15,10 +15,14 @@
 // fetch in async await
 
 const Posts = async () => {
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await posts.json();
-  // console.log(data[0].title);
-  return data[0].title;
+  try {
+    const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await posts.json();
+    // console.log(data[0].title);
+    return data[0].title;
+  } catch (err) {
+    console.log(err);
+  }
 };
 const ShowPost = async () => {
   const value = await Posts();
