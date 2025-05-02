b96df0e548f4958b4d771e848356aa34f8564a7a
raz421 <maharazbhuiyan3@gmail.com>
2025-05-02 14:52:32 +0600

    promise end

--- js referesher/js asynchronous/async.js
+++ js referesher/js asynchronous/async.js
@@ -1,33 +1,27 @@
-const promise = new Promise((resolve, reject) => {
-  const err = false;
-  setTimeout(() => {
-    if (!err) {
-      resolve({
-        user_name: "Meharaz",
-        id: "7889",
-        skill: "react and nextjs",
-        gretting: () => {
-          console.log("Hello developper");
-        },
-      });
-    } else {
-      reject(Error("promise is rejected"));
-    }
-  }, 1000);
-});
-promise
-  .then((res) => {
-    console.log(res);
-    return res.gretting;
-  })
-  .then((gretting) => {
-    gretting();
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-async function dev() {
-  const take = await promise;
-  return take;
-}
-dev();
+// fetch data help of promise chaining
+// const fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
+// fetchPromise
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log( data[0].title);
+//   })
+//   .catch((err) => {
+//     console.log(`the requst has error ${err}`);
+//   });
+// console.log(fetchPromise);
+
+// fetch in async await
+
+const Posts = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await posts.json();
+  // console.log(data[0].title);
+  return data[0].title;
+};
+const ShowPost = async () => {
+  const value = await Posts();
+  console.log(value);
+};
+ShowPost();
