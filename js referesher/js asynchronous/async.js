ede819e139d787cd0809448e1d8d21e1466472da
raz421 <maharazbhuiyan3@gmail.com>
2025-05-02 15:24:53 +0600

    promise example more

--- js referesher/js asynchronous/async.js
+++ js referesher/js asynchronous/async.js
@@ -1,68 +1,33 @@
-// promise in js
-const promise = new Promise((res, rej) => {
+const promise = new Promise((resolve, reject) => {
+  const err = false;
   setTimeout(() => {
-    console.log("async task  is complete ");
-    res();
+    if (!err) {
+      resolve({
+        user_name: "Meharaz",
+        id: "7889",
+        skill: "react and nextjs",
+        gretting: () => {
+          console.log("Hello developper");
+        },
+      });
+    } else {
+      reject(Error("promise is rejected"));
+    }
   }, 1000);
 });
-promise.then(() => {
-  console.log("Promise is consumed");
-});
-// 2
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    console.log("promise 2 async");
-    resolve();
-  }, 2000);
-}).then(() => {
-  console.log("promise 2 consume");
-});
-// 3
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve({ name: "Meharaz", id: "221-15-5883" });
-  }, 2000);
-}).then((user) => {
-  console.log(user);
-});
-// 4
-const promise4 = new Promise((resolve, reject) => {
-  const error = false;
-  setTimeout(() => {
-    if (!error) {
-      resolve({ user_name: "Meharaz", pass: "123" });
-    } else {
-      reject(Error("error detected"));
-    }
-  }, 2000);
-});
-promise4
-  .then((user) => {
-    console.log(user);
-    return user.user_name;
+promise
+  .then((res) => {
+    console.log(res);
+    return res.gretting;
   })
-  .then((userName) => {
-    console.log(userName);
+  .then((gretting) => {
+    gretting();
   })
   .catch((err) => {
     console.log(err.message);
   });
-const promise5 = new Promise((resolve, reject) => {
-  const error = false;
-  setTimeout(() => {
-    if (!error) {
-      resolve({ user_name: "JavaScript", pass: "jsx" });
-    } else {
-      reject(Error("error in js project"));
-    }
-  }, 2000);
-});
-async function responsePromise5() {
-  try {
-    const res = await promise5;
-    console.log(res.user_name);
-  } catch (err) {
-    console.log(err.message);
-  }
+async function dev() {
+  const take = await promise;
+  return take;
 }
-responsePromise5();
+dev();
